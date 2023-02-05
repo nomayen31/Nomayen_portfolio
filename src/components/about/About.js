d8fc1633ba5f58520614d9688fbd1eaed539e956
nomayen <nomayen.ohin@gmail.com>
2023-02-05 10:06:51 +0600

    about me work

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,10 +1,40 @@
 import React from 'react';
 import './About.css'
-
+import ME from '../../assets/about1.png'
+import {FaAward}  from 'react-icons/fa'
+import {FiUsers}  from 'react-icons/fi'
 const About = () => {
     return (
        <section id='about'>
-        About
+      <h5>Get To Know</h5>
+      <h2>about Me</h2>
+
+      <div className='container about__container'>
+        <div className='about__me'>
+            <div className='about__me-image'>
+                <img src={ME} alt='About Image'></img>
+            </div>
+        </div>
+        <div className='about__container'>
+            <div className='about__cards'>
+                <article className='about__card'>
+                    <FaAward className='about__icon'/>
+                    <h5>Experience</h5>
+                    <small>2+ Years Working</small>
+                </article>
+                <article className='about__card'>
+                    <FiUsers className='about__icon'/>
+                    <h5>Experience</h5>
+                    <small>2+ Years Working</small>
+                </article>
+                <article className='about__card'>
+                    <FaAward className='about__icon'/>
+                    <h5>Experience</h5>
+                    <small>2+ Years Working</small>
+                </article>
+            </div>
+        </div>
+      </div>
        </section>
     );
 };
