3575387a699d4c8fc031aa3a17f2481b61d768fa
nomayen <nomayen.ohin@gmail.com>
2023-02-08 22:35:05 +0600

    experiance font and back

--- src/components/experiance/Experiance.js
+++ src/components/experiance/Experiance.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import './Experiance.css'
-import {AiFillHtml5} from 'react-icons/ai'
+import { AiFillHtml5 } from 'react-icons/ai'
 import HTML from '../../assets/icon/html.svg'
 import CSS from '../../assets/icon/css.svg'
-// import HTML from '../../assets/icon/html.svg'
-// import HTML from '../../assets/icon/html.svg'
-// import HTML from '../../assets/icon/html.svg'
+import Bootstrap from '../../assets/icon/bootstarp.jpg'
+import Tailwind from '../../assets/icon/telwindsvg.svg'
+import javascript from '../../assets/icon/js.svg'
+import Node from '../../assets/icon/node.svg'
+import mongo from '../../assets/icon/mongo.svg'
+import express from '../../assets/icon/express.svg'
+import sql from '../../assets/icon/sql.svg'
+import Php from '../../assets/icon/php.svg'
+import react from '../../assets/icon/react.svg'
+
 
 const Experiance = () => {
     return (
@@ -15,76 +22,72 @@ const Experiance = () => {
 
             <div className='container experience__container'>
                 <div className='experience__frontend'>
-                        <h3>Frontend Development</h3>
-                        <div className='experience__content'>
-                            <article className='experiance__details'>
+                    <h3>Frontend Development</h3>
+                    <div className='experience__content'>
+                        <article className='experiance__details'>
                             <img src={HTML} alt='nomayen hossain ohin' />
                             <h4>HTML</h4>
                             <small className='text-light'>Experienced</small>
-                            </article>
-                            <article className='experiance__details'>
+                        </article>
+                        <article className='experiance__details'>
                             <img src={CSS} alt='nomayen hossain ohin' />
                             <h4>CSS</h4>
                             <small className='text-light'>Experienced</small>
-                            </article>
-                            <article className='experiance__details'>
-                            <img src={HTML} alt='nomayen hossain ohin' />
+                        </article>
+                        <article className='experiance__details'>
+                            <img src={Bootstrap} alt='nomayen hossain ohin' />
                             <h4>JavaScript</h4>
                             <small className='text-light'>Experienced</small>
-                            </article>
-                            <article className='experiance__details'>
-                            <img src={HTML} alt='nomayen hossain ohin' />
+                        </article>
+                        <article className='experiance__details'>
+                            <img src={Tailwind} alt='nomayen hossain ohin' />
                             <h4>Bootstrap</h4>
                             <small className='text-light'>Experienced</small>
-                            </article>
-                            <article className='experiance__details'>
-                            <img src={HTML} alt='nomayen hossain ohin' />
+                        </article>
+                        <article className='experiance__details'>
+                            <img src={javascript} alt='nomayen hossain ohin' />
                             <h4>Tailwind</h4>
                             <small className='text-light'>Experienced</small>
-                            </article>
-                            <article className='experiance__details'>
-                            <img src={HTML} alt='nomayen hossain ohin' />
+                        </article>
+                        <article className='experiance__details'>
+                            <img src={react} alt='nomayen hossain ohin' />
                             <h4>React</h4>
                             <small className='text-light'>Experienced</small>
-                            </article>
-                        </div>
+                        </article>
+                    </div>
                 </div>
                 <div className='experience__backend'>
-                <div className='experience__frontend'>
+                    <div className='experience__frontend'>
                         <h3>Frontend Development</h3>
                         <div className='experience__content'>
                             <article className='experiance__details'>
-                            <img src={HTML} alt='nomayen hossain ohin' />
-                            <h4>HTML</h4>
-                            <small className='text-light'>Experienced</small>
+                                <img src={Node} alt='nomayen hossain ohin' />
+                                <h4>Node JS</h4>
+                                <small className='text-light'>Experienced</small>
                             </article>
                             <article className='experiance__details'>
-                            <img src={HTML} alt='nomayen hossain ohin' />
-                            <h4>CSS</h4>
-                            <small className='text-light'>Experienced</small>
+                                <img src={mongo} alt='nomayen hossain ohin' />
+                                <h4>MongoDB</h4>
+                                <small className='text-light'>Experienced</small>
                             </article>
                             <article className='experiance__details'>
-                            <img src={HTML} alt='nomayen hossain ohin' />
-                            <h4>JavaScript</h4>
-                            <small className='text-light'>Experienced</small>
+                                <img src={express} alt='nomayen hossain ohin' />
+                                <h4>Express Js</h4>
+                                <small className='text-light'>Experienced</small>
                             </article>
                             <article className='experiance__details'>
-                            <img src={HTML} alt='nomayen hossain ohin' />
-                            <h4>Bootstrap</h4>
-                            <small className='text-light'>Experienced</small>
+                                <img src={sql} alt='nomayen hossain ohin' />
+                                <h4>Mysql</h4>
+                                <small className='text-light'>Experienced</small>
                             </article>
                             <article className='experiance__details'>
-                            <img src={HTML} alt='nomayen hossain ohin' />
-                            <h4>Tailwind</h4>
-                            <small className='text-light'>Experienced</small>
+                                <img src={Php} alt='nomayen hossain ohin' />
+                                <h4>PHP</h4>
+                                <small className='text-light'>intermediate</small>
                             </article>
-                            <article className='experiance__details'>
-                            <img src={HTML} alt='nomayen hossain ohin' />
-                            <h4>React</h4>
-                            <small className='text-light'>Experienced</small>
-                            </article>
+                           
                         </div>
-                </div>
+                    </div>
                 </div>
             </div>
         </section>
